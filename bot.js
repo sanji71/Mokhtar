69d3271f4505eaed6496de006c3627da17d81509
sanji71 <58048992+sanji71@users.noreply.github.com>
2020-01-13 11:50:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`KiRaDz`,"http://twitch.tv/KiRaDz")
+client.user.setGame(`(Px)`,"http://twitch.tv/(Px)")
 client.user.setStatus("dnd")
 });
 
